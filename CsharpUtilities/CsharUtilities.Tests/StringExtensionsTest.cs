5abb19662be02b43748dcb250fbc83d55f25088c
Adriano Ueda <adriano.ueda@gmail.com>
2015-07-05 22:48:48 -0400

    Added a unit test project using the Xunit framework.

---
+++ CsharpUtilities/CsharUtilities.Tests/StringExtensionsTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using CsharpUtilities.Extensions;
+using Xunit;
+
+namespace CsharUtilities.Tests
+{
+    public class StringExtensionsTest
+    {
+        [Fact]
+        public void NullStringReturnsStringEmpty()
+        {
+            var expected = string.Empty;
+            var actual = StringExtensions.ToFriendlyString(null);
+
+            Assert.Equal<string>(expected, actual);
+        }
+
+        [Fact]
+        public void DoubleDashesReturnsOneDash()
+        {
+            var actual = StringExtensions.ToFriendlyString("word1--word2");
+            var expected = "word1-word2";
+
+            Assert.Equal<string>(expected, actual);
+        }
+
+        [Fact]
+        public void DoubleSpacesReturnsOneDash()
+        {
+            var actual = StringExtensions.ToFriendlyString("word1  word2");
+            var expected = "word1-word2";
+
+            Assert.Equal<string>(expected, actual);
+        }
+
+        [Fact]
+        public void TrailingSpaceReturnsOneDash()
+        {
+            var actual = StringExtensions.ToFriendlyString(" word ");
+            var expected = "-word-";
+
+            Assert.Equal<string>(expected, actual);
+        }   
+
+        [Fact]
+        public void TrailingSpacesReturnsOneDash()
+        {
+            var actual = StringExtensions.ToFriendlyString("   word  ");
+            var expected = "-word-";
+
+            Assert.Equal<string>(expected, actual);
+        }   
+    }
+}
